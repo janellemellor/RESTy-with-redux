8e3c1186f36217f2b65c253db1bc0fc74dabeebf
janelle mellor <janellemellor@gmail.com>
2020-05-14 14:40:10 -0700

    add form presentational component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { RadioButtonGroup, RadioButton } from './RadioButton';
+
+const Form = () => {
+
+  return (
+    <form onSubmit={onSubmit}>
+      <fieldset>
+        <input name="url" type="text" value={url} onChange={onChange} />
+        <RadioButtonGroup name="method" onChange={onChange} >
+          <RadioButton value="GET" />
+          <RadioButton value="POST" />
+          <RadioButton value="PUT" />
+          <RadioButton value="PATCH" />
+          <RadioButton value="DELETE" />
+        </RadioButtonGroup>
+        <button>Go!</button>
+        <textarea name="jsonBody" value={jsonBody} onChange={onChange}> </textarea>
+      </fieldset>
+    </form>
+  );
+};
+ 
+export default Form;
