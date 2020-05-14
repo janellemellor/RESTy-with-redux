7989595bab2248728a9ecd6648b00099722b0447
janelle mellor <janellemellor@gmail.com>
2020-05-14 15:00:05 -0700

    add provider to form snapshot

--- src/components/Form/Form.test.jsx
+++ src/components/Form/Form.test.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Form from './Form';
+import { RESTyProvider } from '../../hooks/RESTyProvider';
 
 describe('Form Component', () => {
   it('matches a snapshot', () => {
-    const wrapper = shallow(<Form onSubmit={() => {}} url="https://github.com/janellemellor" onChange={() => {}} jsonBody="{}" />);
+    const wrapper = shallow(
+    <RESTyProvider>
+      <Form onSubmit={() => {}} url="https://github.com/janellemellor" onChange={() => {}} jsonBody="{}" />
+    </RESTyProvider>);
     expect(wrapper).toMatchSnapshot();
   });
 }); 
