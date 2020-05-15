c17f47d9cacd09548717b83fcfcbc922fa31c0fa
janelle mellor <janellemellor@gmail.com>
2020-05-15 08:07:20 -0700

    update response test with provider

--- src/components/Response/Response.test.jsx
+++ src/components/Response/Response.test.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Response from './Response';
+import { RESTyProvider } from '../../hooks/RESTyProvider';
 
 describe('Response Component', () => {
   it('matches a snapshot', () => {
     const wrapper = shallow(
-      <Response response='{}' />
+      <RESTyProvider>
+        <Response response='{}' />
+      </RESTyProvider>
     );
     expect(wrapper).toMatchSnapshot();
   });  
