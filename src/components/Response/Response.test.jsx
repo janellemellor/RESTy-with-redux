9a4f264421eb2926433d6fdb395dd3f0ca7371c5
janelle mellor <janellemellor@gmail.com>
2020-05-14 17:45:11 -0700

    add snapshot test for Response

---
+++ src/components/Response/Response.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Response from './Response';
+
+describe('Response Component', () => {
+  it('matches a snapshot', () => {
+    const wrapper = shallow(
+      <Response response='{}' />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });  
+});
