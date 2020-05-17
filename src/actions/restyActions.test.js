4085bd047cd3534de42b191d0cd48653c1540618
janelle mellor <janellemellor@gmail.com>
2020-05-17 09:39:52 -0700

    fix test

--- src/actions/restyActions.test.js
+++ src/actions/restyActions.test.js
@@ -3,13 +3,13 @@
 //TODO: add an action to set the response from the request into state- test is incomplete
 
 describe.skip('resty actions', () => {
-  it('creates a set request action', () => {
-    const action = setRequest({
-      request: {
-    }});
+//   it('creates a set request action', () => {
+//     const action = setRequest({
+//       request: {
+//     }});
 
-    expect(action).toEqual({
-      type: SET_REQUEST,
-    });
-  });  
+//     expect(action).toEqual({
+//       type: SET_REQUEST,
+//     });
+//   });  
 })
