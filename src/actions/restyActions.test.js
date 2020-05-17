874292e9b4ca8fd958527f7c3abb861cd8592b94
janelle mellor <janellemellor@gmail.com>
2020-05-17 09:31:35 -0700

    update actions test

--- src/actions/restyActions.test.js
+++ src/actions/restyActions.test.js
@@ -1,18 +1,18 @@
 //TODO: add an action to set the values of the request from the form into state (input- url, method, body)
 
-//TODO: add an action to set the response from the request into state
+//TODO: add an action to set the response from the request into state- test is incomplete
 
-// describe('resty actions', () => {
-//   it('creates a set request action', () => {
-//     const action = setRequest({
-//       request: {
-//     });
-//     expect(action).toEqual({
-//       type: SET_REQUEST,
-//       payload: [{
-//         request: {
-//         }
-//     }];
-//     });
-//   });  
-// })
+describe.skip('resty actions', () => {
+  it('creates a set request action', () => {
+    const action = setRequest({
+      request: {
+    });
+    expect(action).toEqual({
+      type: SET_REQUEST,
+      payload: [{
+        request: {
+        }
+    }];
+    });
+  });  
+})
